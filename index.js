13961da8417b0c5a295c6f00f04a510dcf8195d8
atneon27 <aryan.tiwari.at27@gmail.com>
2024-12-02 23:29:20 +0530

    functionality to add tasks

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { Command } from "commander";
+const program = new Command();
+
+let task_storage = {};
 
 program
-    .version("1.0.0")
-    .description("Todo CLI")
-    .option("-n, --name <type>", "Add your name")
-    .action((option) => {
-        console.log(option.name)
-    })
+  .name("todo-cli")
+  .description("Todo CLI")
+  .version("1.0.0")
 
-program.parse(process.argv)
+program.command("add")
+  .description("Add task/tasks")
+  .argument('<tasks...>', 'Task to add')
+  .action((tasks) => {
+    const date = new Date().toLocaleDateString();
+    if (!task_storage[date]) {
+        task_storage[date] = [];
+    }
+    task_storage[date].push(...tasks);
+    console.log(task_storage)
+  });
 
+  program.parse();
