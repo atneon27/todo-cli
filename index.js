cb182ea2945c334da1c9caa3bfeb8b44c44582f9
atneon27 <aryan.tiwari.at27@gmail.com>
2024-12-04 23:35:44 +0530

    improved task command logic

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import fs from 'fs'
+import fs from 'fs';
+
+// we can use fs/promise to use async/await in fs
+
+function updateJSON(path, updateCallback, finalCallback) {
+  fs.readFile(path, 'utf-8', (readErr, dataString) => {
+    if(readErr) {
+      finalCallback(readErr)
+    }
+
+    let object
+    try {
+      object = JSON.parse(dataString)
+    } catch(parsErr) {
+      finalCallback(parsErr)
+    }
+
+    object = updateCallback(object)
+
+    fs.writeFile(path, JSON.stringify(object), 'utf-8', (writeErr) => {
+      if(writeErr) {
+        finalCallback(writeErr)
+      } else {
+        finalCallback(null, "Success")
+      }
+    })
+  })
+}
 
 const program = new Command();
 
@@ -47,6 +74,43 @@ program.command("global")
         console.log(err)
       } else {
         console.log(JSON.parse(taskString))
+      }
+    })
+  })
+
+program.command("task")
+  .option('-t, --title <title>', 'Task Title')
+  .option('-d, --description <description...>', 'Task Descriptiond')
+  .action((option) => {
+    if(!option.title)  {
+      console.log("No task title passed!")
+      return
+    }
+    else if(!option.description) {
+      console.log("No task description passed!")
+      return
+    }
+
+    const date = new Date().toLocaleDateString()
+
+    updateJSON('./task.json', (object) => {
+      if(!object.global[date]) {
+        object.global[date] = []
+      }
+
+      object.global[date].push({
+        id: 1,
+        title: option.title,
+        description: option.description.join(" "),
+        completed: false
+      })
+
+      return object
+    }, (err, msg) => {
+      if(err) {
+        console.error(err)
+      } else {
+        console.log(msg)
       }
     })
   })
